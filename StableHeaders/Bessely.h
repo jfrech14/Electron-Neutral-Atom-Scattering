d14bbfeb7946bd0a22fe44afda5ecb99f16f9a93
Josh Frechem <jfrech14@gmail.com>
2019-04-13 23:18:21 -0400

    Bessel y_n Recursion Routine
    tested

---
+++ StableHeaders/Bessely.h
@@ -0,0 +1,23 @@
+#ifndef Bessely_H
+#define Bessely_H
+
+    double Bessely(int l, double x)
+    {
+        double y2;
+        double y0=-cos(x)/x;
+        double y1=y0/x-sin(x)/x;
+        if(l==0)
+            return y0;
+        if(l==1)
+            return y1;
+
+        for(int i=2;i<=l;++i)
+        {
+            y2=(2.0*i-1)/x*y1-y0;
+            y0=y1;
+            y1=y2;
+        }
+        return(y2);
+    }
+
+#endif
